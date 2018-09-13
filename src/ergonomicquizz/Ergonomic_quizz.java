3cc7ed4e8295f1eb6a2f0f648915328c0d70418a
Jun Yamada <jun3120@gmail.com>
2018-09-13 08:52:37 -0300

    quizz

---
+++ src/ergonomicquizz/Ergonomic_quizz.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ergonomicquizz;
+
+
+/**
+ *
+ * @author yamada
+ */
+public class Ergonomic_quizz {
+        
+    public static void main(String[] args) {
+            
+       Init i = new Init();
+       i.setTitle("Bem-Vindo(a)!");
+       i.setLocationRelativeTo(null);
+       i.setVisible(true);
+    }
+}
